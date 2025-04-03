a012e47684e314fb80fd9c7ad15060c591c730a1
Bodhitha Weerasekara <88415912+IT20204334@users.noreply.github.com>
2025-04-03 08:15:35 +0530

    service

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,35 +1,3 @@
-// import React from "react";
-
-// export const Services = (props) => {
-//   return (
-//     <div id="services" className="text-center">
-//       <div className="container">
-//         <div className="section-title">
-//           <h2>Our Services</h2>
-//           <p>
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-//             dapibus leonec.
-//           </p>
-//         </div>
-//         <div className="row">
-//           {props.data
-//             ? props.data.map((d, i) => (
-//                 <div key={`${d.name}-${i}`} className="col-md-4">
-//                   {" "}
-//                   <i className={d.icon}></i>
-//                   <div className="service-desc">
-//                     <h3>{d.name}</h3>
-//                     <p>{d.text}</p>
-//                   </div>
-//                 </div>
-//               ))
-//             : "loading"}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
 import React from "react";
 
 export const Services = (props) => {
@@ -39,8 +7,9 @@ export const Services = (props) => {
         <div className="section-title">
           <h2 className="topic">Our Services</h2>
           <p className="text-muted">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          Transform your space with our top-tier cleaning services! Whether it’s schools, homes, or corporate offices, 
+          we bring a spotless shine to every corner. Our dedicated team ensures a fresh, hygienic, and welcoming environment, 
+          so you can focus on what truly matters. Let us handle the mess while you enjoy the cleanliness!
           </p>
         </div>
         <div className="row">
